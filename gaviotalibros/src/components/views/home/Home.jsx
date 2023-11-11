706b1b8513006d71ebe27f3aa7566e009bbdbbac
onill <onilmc@gmail.com>
2023-11-10 22:32:10 -0300

    changes new

---
+++ gaviotalibros/src/components/views/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <>
+      <p>Soy Home...por aqui comenzamos con el codigo..
+      </p>  
+    </>
+  );
+}
+
+export default Home;
+
